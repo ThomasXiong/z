42ec85795deffd384c1b3cca314487adfabfcea2
Thomas Xiong <txiong11@uncc.edu>
2019-10-24 19:51:46 +0000

    set up creation

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -3,4 +3,11 @@ class ExamsController < ApplicationController
     def new
         
     end
+    
+    def create
+        
+        render plain: params[:exams].inspect
+        
+    end
+    
 end
