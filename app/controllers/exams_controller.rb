9c0b73e2da1c9c3f140f5f572ddd732fff17a6e6
Thomas Xiong <txiong11@uncc.edu>
2019-10-31 22:50:33 +0000

    delete function added

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -28,6 +28,30 @@ class ExamsController < ApplicationController
         @exams = Exam.find(params[:id])
     end
     
+    def edit
+    
+    @exams = Exam.find(params[:id])
+    
+    end
+    
+    def update 
+        
+        @exams = Exam.find(params[:id])
+        if @exams.update(exams_params)
+            redirect_to @exams
+        else
+            render 'edit'
+        end 
+    end
+    
+    def destroy
+    
+    @exams = Exam.find(params[:id])
+    @exams.destroy
+    redirect_to exams_path
+     
+    end
+    
     private
     def exams_params
         params.require(:exams).permit(:name, :student_number)
