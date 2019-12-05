d1e986831b207ff3f0c2e7258512afcdd59564f0
Thomas Xiong <txiong11@uncc.edu>
2019-12-05 20:54:54 +0000

    updated

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,9 +1,9 @@
 class Exam < ApplicationRecord
    
-    validates :name, presence: true, length: {maximum: 20}, format: { with: /\A[a-zA-Z ]+\z/x,
+    validates :name, presence: true, length: {maximum: 7}, format: { with: /\A[a-zA-Z0-9\s]+\z/i,
     message: "can only be letters" }
     
-    validates :student_number, presence: true, length: {is: 9}, format: { with: /\A[+-]?\d+\z/,
+    validates :student_number, presence: true, length: {maximum: 2}, format: { with: /\A[+-]?\d+\z/,
     message: "can only numbers" }
      
 end
